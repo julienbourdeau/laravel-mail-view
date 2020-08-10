2da1abc6dd67a0fe3ee929e6a80feecf201da0dd
Julien Bourdeau <julien@sigerr.org>
2020-08-10 22:34:12 +0200

    Use route config prefix

--- src/routes.php
+++ src/routes.php
@@ -5,8 +5,10 @@ use Julienbourdeau\MailView\Authorize;
 
 Route::middleware(Authorize::class)->name('mail-view.')->group(function () {
 
-    Route::get('mail_view', [MailViewController::class, 'index'])->name('index');
+    $urlPrefix = rtrim(config('mail-view.url_prefix'), '/');
 
-    Route::get('mail_view/{class_name}/{method_name}', [MailViewController::class, 'show'])->name('show');
+    Route::get($urlPrefix, [MailViewController::class, 'index'])->name('index');
+
+    Route::get($urlPrefix.'/{class_name}/{method_name}', [MailViewController::class, 'show'])->name('show');
 
 });
